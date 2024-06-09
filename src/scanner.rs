541594f0c1fdb0ba7aba711c4f0aa06b18a5dd51
sik1j <karkisikij2@gmail.com>
2024-06-08 20:47:23 -0700

    Create basic token types

---
+++ src/scanner.rs
@@ -0,0 +1,41 @@
+use std::fmt::{Formatter};
+
+#[derive(Debug)]
+pub enum TokenType {
+    // Single-character tokens.
+    LeftParen, RightParen, LeftBrace, RightBrace,
+    Comma, Dot, Minus, Plus, Semicolon, Slash, Star,
+
+    // One or two character tokens.
+    Bang, BangEqual,
+    Equal, EqualEqual,
+    Greater, GreaterEqual,
+    Less, LessEqual,
+
+    // Literals.
+    Identifier, String, Number,
+
+    // Keywords.
+    And, Class, Else, False, Fun, For, If, Nil, Or,
+    Print, Return, Super, This, True, Var, While,
+
+    Eof
+}
+
+pub struct Token {
+    token_type: TokenType,
+    lexeme: String,
+    line: usize,
+}
+
+impl Token {
+    pub fn new(token_type: TokenType, lexeme: String, line: usize) -> Self {
+        Token {token_type, lexeme, line}
+    }
+}
+
+impl std::fmt::Display for Token {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?} {} {}", self.token_type, self.lexeme, self.line)
+    }
+}
